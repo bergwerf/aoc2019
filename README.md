55e1c1da949919e2075ce7be94d1cc46013459c8
Herman Bergwerf <hermanbergwerf@gmail.com>
2019-12-02 21:48:38 +0100

    Add day 1 solution

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Advent of Code 2019
 ===================
 This year I want to learn several new languages and this seems like a good 
-opportunity to do so! I want to learn Ada and Crystal, and each day I will
-pick the language that is still using the fewest lines of code in total.
-I will start with Ada.
+opportunity to do so! I want to learn various languages like Ada and Crystal. If
+I really like a language I might even stick to it. To prevent clutter I added a
+separate folder for each day.
---
+++ day1/day1.adb
@@ -0,0 +1,39 @@
+with Ada.strings, Ada.text_io;
+use Ada.strings, Ada.text_io;
+
+procedure Day1 is
+   file : File_Type;
+   mass, fuel : Float;
+   sum : Integer;
+begin
+   -- Open input.txt
+   open(file=>file, mode=>In_File, name=>"input.txt");
+
+   -- Part 1
+   sum := 0;
+   while not end_of_file(file) loop
+      mass := Float'value(get_line(file));
+      fuel := Float'floor(mass / 3.0 - 2.0);
+      sum := sum + Integer(fuel);
+   end loop;
+   put_line("Fuel part 1:" & Integer'image(sum));
+
+   -- Part 2
+   sum := 0;
+   reset(file);
+   while not end_of_file(file) loop
+      mass := Float'value(get_line(file));
+      loop
+         fuel := Float'floor(mass / 3.0 - 2.0);
+         mass := fuel;
+         if fuel > 0.0 then
+            sum := sum + Integer(fuel);
+         else
+            exit;
+         end if;
+      end loop;
+   end loop;
+   put_line("Fuel part 2:" & Integer'image(sum));
+
+   close(file);
+end Day1;
